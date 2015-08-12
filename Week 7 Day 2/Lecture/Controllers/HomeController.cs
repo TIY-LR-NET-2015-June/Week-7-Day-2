c078515c3d93a8d8763f05274536884e66847396
Daniel Pollock <daniellpollock@gmail.com>
2015-08-12 07:47:09 -0500

    added lecture code

---
+++ Week 7 Day 2/Lecture/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lecture.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string yourname)
+        {
+            Thread.Sleep(2000); 
+            return PartialView("MessageArea", yourname);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
